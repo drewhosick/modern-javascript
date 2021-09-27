cf6fc06306aa56496ab03118d199552299bc905b
drewhosick <drewhosick@gmail.com>
2021-09-26 20:10:04 -0400

    Completed JS and main CSS work on weather app

---
+++ weather_app/scripts/app.js
@@ -0,0 +1,67 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (data) => {
+    // const cityDetails = data.cityDetails;
+    // const weather = data.weather;
+
+    // destructuring example 
+    const { cityDetails, weather } = data;
+
+    // update details template
+
+    details.innerHTML = `
+        <h5 class="my-3">${cityDetails.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+          <span>${weather.Temperature.Metric.Value}</span>
+          <span>&deg;C</span>
+        </div>
+    `;
+
+    // remove d-none class if present
+
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+
+    // update night and day and icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+    
+    let timeSrc = null;
+    if(weather.isDayTime) {
+        timeSrc = 'img/day.svg';
+    } else {
+        timeSrc = 'img/night.svg';
+    }
+    time.setAttribute('src', timeSrc);
+
+
+
+}
+
+const updateCity = async (city) => {
+
+    const cityDetails = await getCity(city);
+    const weather = await getWeather(cityDetails.Key);
+
+    return {cityDetails, weather};
+};
+
+cityForm.addEventListener('submit', e => {
+    // prevent default action
+    e.preventDefault();
+    
+    // get city value from form and reset it
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    // update the ui with the new city
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+});
